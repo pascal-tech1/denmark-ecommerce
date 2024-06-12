df52286bbd5c67a5b5daa4711475e8142097e05b
Pascal <pascalazubike003@gmail.com>
2024-06-12 22:00:18 +0100

    remove all componets

--- src/components/components/Container.jsx
+++
@@ -1,9 +0,0 @@
-const Container = ({ children, className }) => {
-  return (
-    <div className={`${className} max-w-screen-xl mx-auto px-4 xl:px-0 py-10`}>
-      {children}
-    </div>
-  );
-};
-
-export default Container;
