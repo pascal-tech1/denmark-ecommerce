2bee5fa4d5db8c0f6050e0af0b60c3f647744dd4
Azubike Pascal <pascalazubike004@gmail.com>
2024-07-11 12:22:19 +0100

    added useEFfect import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,6 +10,7 @@ import { useQuery } from "@tanstack/react-query";
 
 import { CategorySuggestion, CategorySuggestionSheet } from "@/components/admin-panel/categorySuggestionSheet";
 import { useState } from 'react';
+import useEffect from 'react';
 
 export default function DashboardPage() {
   const [showFeatures, setShowFeatures] = useState(false);
