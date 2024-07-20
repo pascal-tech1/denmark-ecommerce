fa6418fa32c35c222937452ddfb4cd54b8483ba1
Pascal <pascalazubike003@gmail..com>
2024-07-20 14:45:58 -0700

    denmark display on social media when copied

--- src/app/productdetail/[id]/page.tsx
+++ src/app/productdetail/[id]/page.tsx
@@ -21,13 +21,13 @@ export const metadata: Metadata = {
     description:
       " accessories, and more, all with free shipping.",
     type: "website",
-    images:
-    {
-      url: 'https://www.denmarkmultibuzltd.com/_next/image?url=https%3A%2F%2Fres.cloudinary.com%2Fdztt3ldiy%2Fimage%2Fupload%2Fv1721251502%2FWhatsApp_Image_2024-07-09_at_4.50.59_AM_snpybj.jpg&w=640&q=75',
-      width: 1200,
-      height: 630,
-      alt: "Men's Fashion Store"
-    }
+    images: [
+      {
+        url: 'https://www.denmarkmultibuzltd.com/_next/image?url=https%3A%2F%2Fres.cloudinary.com%2Fdztt3ldiy%2Fimage%2Fupload%2Fv1721251502%2FWhatsApp_Image_2024-07-09_at_4.50.59_AM_snpybj.jpg&w=640&q=75',
+        width: 300,
+        height: 300,
+        alt: "Men's Fashion Store"
+      }]
 
   },
 
