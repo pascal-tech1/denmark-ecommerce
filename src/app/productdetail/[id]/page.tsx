44cb9edda4b3cffea3430d777f08b30d30e3993f
Pascal <pascalazubike003@gmail..com>
2024-07-20 13:12:03 -0700

    turned productdetail page to server component

--- src/app/productdetail/[id]/page.tsx
+++ src/app/productdetail/[id]/page.tsx
@@ -16,7 +16,7 @@ export default function ProductDetailPage() {
         <meta property="og:site_name" content="Pure Surface" />
         <meta property="og:title" content="225 Days of Self Sanitizing Surface from Coronavirus" />
         <meta property="og:description" content="Just 1 time application" />
-        <meta property="og:image" itemprop="image" content="https://www.denmarkmultibuzltd.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fbannerone.f814d19f.png&w=3840&q=75" />
+        <meta property="og:image" itemProp="image" content="https://www.denmarkmultibuzltd.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fbannerone.f814d19f.png&w=3840&q=75" />
         <meta property="og:type" content="website" />
         <meta property="og:image:type" content="image/png" />
         <meta property="og:image:width" content="300" />
