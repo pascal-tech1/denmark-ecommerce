d8e59a8db2529504568199fdb218e6cc41f90d26
Pascal <pascalazubike003@gmail..com>
2024-07-20 13:09:02 -0700

    turned productdetail page to server component

--- src/app/productdetail/[id]/page.tsx
+++ src/app/productdetail/[id]/page.tsx
@@ -7,51 +7,21 @@ import Head from "next/head";
 export default function ProductDetailPage() {
 
 
-  // const metadata: Metadata = {
-  //   metadataBase: new URL(
-  //     process.env.APP_URL
-  //       ? `${process.env.APP_URL}`
-  //       : process.env.VERCEL_URL
-  //         ? `https://${process.env.VERCEL_URL}`
-  //         : `http://localhost:${process.env.PORT || 3000}`
-  //   ),
-  //   title: "Denmark Single Product Page",
-  //   description:
-  //     "just found this wonderfull product available on denmarkmulbizltd.",
-  //   alternates: {
-  //     canonical: "/"
-  //   },
-  //   openGraph: {
-  //     url: "https://www.denmarkmultibuzltd.com/productdetail/669837055c645243fae405cc",
-  //     title: "title in openGraph",
-  //     description:
-  //       " accessories, and more, all with free shipping.",
-  //     type: "website",
-  //     images:
-  //     {
-  //       url: 'https://www.denmarkmultibuzltd.com/_next/image?url=https%3A%2F%2Fres.cloudinary.com%2Fdztt3ldiy%2Fimage%2Fupload%2Fv1721251502%2FWhatsApp_Image_2024-07-09_at_4.50.59_AM_snpybj.jpg&w=640&q=75',
-  //       width: 1200,
-  //       height: 630,
-  //       alt: "Men's Fashion Store"
-  //     }
 
-  //   },
-
-
-
-  // };
   return (
 
     <div>
       <Head>
-        <title>this is the title</title>
-        <meta name="description" content={"this is the content"} />
-        <meta property="og:title" content="og title" />
-        <meta property="og:description" content="og description" />
-        <meta property="og:image" content="https://www.denmarkmultibuzltd.com/_next/image?url=https%3A%2F%2Fres.cloudinary.com%2Fdztt3ldiy%2Fimage%2Fupload%2Fv1721251502%2FWhatsApp_Image_2024-07-09_at_4.50.59_AM_snpybj.jpg&w=640&q=75" />
-        <meta property="og:url" content="https://www.denmarkmultibuzltd.com/_next/image?url=https%3A%2F%2Fres.cloudinary.com%2Fdztt3ldiy%2Fimage%2Fupload%2Fv1721251502%2FWhatsApp_Image_2024-07-09_at_4.50.59_AM_snpybj.jpg&w=640&q=75" />
+        <meta name="msapplication-TileImage" content="https://www.denmarkmultibuzltd.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fbannerone.f814d19f.png&w=3840&q=75" />
+        <meta property="og:site_name" content="Pure Surface" />
+        <meta property="og:title" content="225 Days of Self Sanitizing Surface from Coronavirus" />
+        <meta property="og:description" content="Just 1 time application" />
+        <meta property="og:image" itemprop="image" content="https://www.denmarkmultibuzltd.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fbannerone.f814d19f.png&w=3840&q=75" />
         <meta property="og:type" content="website" />
-        <link rel="canonical" href="/name" />
+        <meta property="og:image:type" content="image/png" />
+        <meta property="og:image:width" content="300" />
+        <meta property="og:image:height" content="300" />
+        <meta property="og:url" content="https://www.denmarkmultibuzltd.com/productdetail/6696e7b51043a84424903d2c" />
       </Head>
       <ProductDetail />
     </div>
