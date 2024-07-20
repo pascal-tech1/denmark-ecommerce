07c0796c4bdec3498ded88ee388b25c9c430483b
Pascal <pascalazubike003@gmail..com>
2024-07-20 10:38:13 -0700

    turned productdetail page to server component

--- src/app/productdetail/[id]/page.tsx
+++ src/app/productdetail/[id]/page.tsx
@@ -1,159 +1,43 @@
-"use client";
-import Image from "next/image";
-import { useEffect, useState } from "react";
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator
-} from "@/components/ui/breadcrumb";
-import { ContentLayout } from "@/components/admin-panel/content-layout";
-import Link from "next/link";
-import { Button } from "@/components/ui/button";
-import { cn } from "@/lib/utils";
-import { usePathname } from "next/navigation";
-import { useQuery } from "@tanstack/react-query";
-import axios from "axios";
-import Head from "next/head";
+import ProductDetail from "@/components/admin-panel/productDetailComp";
+import type { Metadata } from "next";
 
-const ProductDetail = () => {
-  const [quantity, setQuantity] = useState(1);
-  const pathname = usePathname();
-  const id = pathname.split("/").pop();
-  const [showFeatures, setShowFeatures] = useState(false);
-
-  const { isPending, error, data } = useQuery({
-    queryKey: [id],
-    queryFn: () => axios(`/routes/fetchSingleProduct?productId=${id}`).then(res => res.data),
-  });
-
-  useEffect(() => {
-    const handleResize = () => {
-      const topDiv = document.querySelector(".top-div") as HTMLElement | null;
-      if (topDiv) {
-        setShowFeatures(topDiv.offsetWidth >= 940);
+export const metadata: Metadata = {
+  metadataBase: new URL(
+    process.env.APP_URL
+      ? `${process.env.APP_URL}`
+      : process.env.VERCEL_URL
+        ? `https://${process.env.VERCEL_URL}`
+        : `http://localhost:${process.env.PORT || 3000}`
+  ),
+  title: "Men's Fashion Store",
+  description:
+    "Discover the latest in men's fashion at our online store. Shop a wide range of clothing, accessories, and more, all with free shipping.",
+  alternates: {
+    canonical: "/"
+  },
+  openGraph: {
+    url: "/",
+    title: "Men's Fashion Store",
+    description:
+      "Discover the latest in men's fashion at our online store. Shop a wide range of clothing, accessories, and more, all with free shipping.",
+    type: "website",
+    images: [
+      {
+        url: `https://denmark-ecommerce.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fbannerone.f814d19f.png&w=3840&q=75`,
+        width: 1200,
+        height: 630,
+        alt: "Men's Fashion Store"
       }
-    };
+    ]
+  },
 
-    handleResize();
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
-
-  const handleAddToCart = () => {
-    console.log(`Added ${quantity} of ${data?.product?.title} to cart`);
-  };
-
-  const handleCheckout = () => {
-    console.log(`Added ${quantity} of ${data?.product?.title} to cart`);
-    console.log("Proceeding to checkout");
-  };
-
-  if (isPending) {
-    return <h1>Loading...</h1>;
-  }
-
-  return (
-    <ContentLayout title="Product Detail">
-      <Head>
-        <title>{data?.product?.title}</title>
-        <meta property="og:title" content={`${data?.product?.title}`} />
-        <meta property="og:description" content={`${data?.product?.description}`} />
-        <meta property="og:image" content={`${data?.product?.imageUrl}`} />
-        <meta property="og:url" content={`https://www.denmarkmultibuzltd.com/productdetail/${data?.product?._id}`} />
-        <meta property="og:type" content="product" />
-      </Head>
-      <Breadcrumb>
-        <BreadcrumbList>
-          <BreadcrumbItem>
-            <BreadcrumbLink asChild>
-              <Link href="/">Home</Link>
-            </BreadcrumbLink>
-          </BreadcrumbItem>
-          <BreadcrumbSeparator />
-          <BreadcrumbItem>
-            <BreadcrumbPage>Product</BreadcrumbPage>
-          </BreadcrumbItem>
-        </BreadcrumbList>
-      </Breadcrumb>
-      <div className="flex justify-center mt-6 items-center min-h-[calc(100vh-56px-64px-20px-24px-56px-48px)]">
-        <div className="lg:p-12">
-          <div
-            className={cn(
-              showFeatures ? "flex-row" : "flex-col",
-              "top-div relative rounded-lg flex gap-12"
-            )}
-          >
-            <div
-              className={cn(
-                showFeatures && "max-w-[700px] relative",
-                "h-full w-full border rounded-lg border-opacity-20"
-              )}
-            >
-              <Image
-                src={data?.product?.imageUrl}
-                alt={data?.product?.title}
-                height={400}
-                width={400}
-                blurDataURL={`data:image/jpeg;base64,${data?.product?.blurImage}`}
-                placeholder="blur"
-                className="object-cover h-full w-full rounded-md transition-transform group-hover:scale-105"
-                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-              />
-            </div>
-            <div className="flex flex-col my-2 gap-6">
-              <h1 className="text-2xl">{data?.product?.title}</h1>
-              <h2 className="text-lg font-semibold">
-                &#8358; {data?.product?.price}
-              </h2>
-              <div>
-                <label className="block">
-                  Quantity:
-                  <input
-                    type="number"
-                    value={quantity}
-                    onChange={(e) => setQuantity(Number(e.target.value))}
-                    min="1"
-                    className="ml-2 w-16 px-2 py-1 border rounded-md"
-                  />
-                </label>
-              </div>
-              <div className="flex gap-6">
-                <Button
-                  variant="default"
-                  onClick={handleAddToCart}
-                  className="bg-yellow-300 rounded-md"
-                >
-                  Add to Cart
-                </Button>
-                <Button
-                  variant="outline"
-                  onClick={handleCheckout}
-                  className="border border-yellow-300 rounded-md"
-                >
-                  Checkout
-                </Button>
-              </div>
-            </div>
-          </div>
-          <div className="mt-8 text-gray-700 dark:text-gray-400 max-w-[900px]">
-            <h3 className="text-xl font-semibold mb-2">Description</h3>
-            <div
-              dangerouslySetInnerHTML={{
-                __html: data?.product.description
-              }}
-            />
-          </div>
-          <div className="mt-8">
-            <h3 className="text-xl font-semibold mb-4">More Products</h3>
-            <div className="">{/* <Products images={imageData} /> */}</div>
-          </div>
-        </div>
-      </div>
-    </ContentLayout>
-  );
 };
 
-export default ProductDetail;
+export default function () {
+  return (
+
+    <div>
+      <ProductDetail />
+    </div>
+  );
+}
---
+++ src/components/admin-panel/productDetailComp.tsx
@@ -0,0 +1,161 @@
+"use client";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import {
+    Breadcrumb,
+    BreadcrumbItem,
+    BreadcrumbLink,
+    BreadcrumbList,
+    BreadcrumbPage,
+    BreadcrumbSeparator
+} from "@/components/ui/breadcrumb";
+import { ContentLayout } from "@/components/admin-panel/content-layout";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { usePathname } from "next/navigation";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import Head from "next/head";
+
+const ProductDetail = () => {
+    const [quantity, setQuantity] = useState(1);
+    const pathname = usePathname();
+    const id = pathname.split("/").pop();
+    const [showFeatures, setShowFeatures] = useState(false);
+
+    const { isPending, error, data } = useQuery({
+        queryKey: [id],
+        queryFn: () => axios(`/routes/fetchSingleProduct?productId=${id}`).then(res => res.data),
+    });
+
+    useEffect(() => {
+        const handleResize = () => {
+            const topDiv = document.querySelector(".top-div") as HTMLElement | null;
+            if (topDiv) {
+                setShowFeatures(topDiv.offsetWidth >= 940);
+            }
+        };
+
+        handleResize();
+        window.addEventListener("resize", handleResize);
+        return () => window.removeEventListener("resize", handleResize);
+    }, []);
+
+    const handleAddToCart = () => {
+        console.log(`Added ${quantity} of ${data?.product?.title} to cart`);
+    };
+
+    const handleCheckout = () => {
+        console.log(`Added ${quantity} of ${data?.product?.title} to cart`);
+        console.log("Proceeding to checkout");
+    };
+
+    if (isPending) {
+        return <h1>Loading...</h1>;
+    }
+
+
+    return (
+        <ContentLayout title="Product Detail">
+
+            <Head>
+                <title>{data?.product?.title}</title>
+                <meta property="og:title" content={`${data?.product?.title}`} />
+                <meta property="og:description" content={`${data?.product?.description}`} />
+                <meta property="og:image" content={`${data?.product?.imageUrl}`} />
+                <meta property="og:url" content={`https://www.denmarkmultibuzltd.com/productdetail/${data?.product?._id}`} />
+                <meta property="og:type" content="product" />
+            </Head>
+            <Breadcrumb>
+                <BreadcrumbList>
+                    <BreadcrumbItem>
+                        <BreadcrumbLink asChild>
+                            <Link href="/">Home</Link>
+                        </BreadcrumbLink>
+                    </BreadcrumbItem>
+                    <BreadcrumbSeparator />
+                    <BreadcrumbItem>
+                        <BreadcrumbPage>Product</BreadcrumbPage>
+                    </BreadcrumbItem>
+                </BreadcrumbList>
+            </Breadcrumb>
+            <div className="flex justify-center mt-6 items-center min-h-[calc(100vh-56px-64px-20px-24px-56px-48px)]">
+                <div className="lg:p-12">
+                    <div
+                        className={cn(
+                            showFeatures ? "flex-row" : "flex-col",
+                            "top-div relative rounded-lg flex gap-12"
+                        )}
+                    >
+                        <div
+                            className={cn(
+                                showFeatures && "max-w-[700px] relative",
+                                "h-full w-full border rounded-lg border-opacity-20"
+                            )}
+                        >
+                            <Image
+                                src={data?.product?.imageUrl}
+                                alt={data?.product?.title}
+                                height={400}
+                                width={400}
+                                blurDataURL={`data:image/jpeg;base64,${data?.product?.blurImage}`}
+                                placeholder="blur"
+                                className="object-cover h-full w-full rounded-md transition-transform group-hover:scale-105"
+                                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                            />
+                        </div>
+                        <div className="flex flex-col my-2 gap-6">
+                            <h1 className="text-2xl">{data?.product?.title}</h1>
+                            <h2 className="text-lg font-semibold">
+                                &#8358; {data?.product?.price}
+                            </h2>
+                            <div>
+                                <label className="block">
+                                    Quantity:
+                                    <input
+                                        type="number"
+                                        value={quantity}
+                                        onChange={(e) => setQuantity(Number(e.target.value))}
+                                        min="1"
+                                        className="ml-2 w-16 px-2 py-1 border rounded-md"
+                                    />
+                                </label>
+                            </div>
+                            <div className="flex gap-6">
+                                <Button
+                                    variant="default"
+                                    onClick={handleAddToCart}
+                                    className="bg-yellow-300 rounded-md"
+                                >
+                                    Add to Cart
+                                </Button>
+                                <Button
+                                    variant="outline"
+                                    onClick={handleCheckout}
+                                    className="border border-yellow-300 rounded-md"
+                                >
+                                    Checkout
+                                </Button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="mt-8 text-gray-700 dark:text-gray-400 max-w-[900px]">
+                        <h3 className="text-xl font-semibold mb-2">Description</h3>
+                        <div
+                            dangerouslySetInnerHTML={{
+                                __html: data?.product.description
+                            }}
+                        />
+                    </div>
+                    <div className="mt-8">
+                        <h3 className="text-xl font-semibold mb-4">More Products</h3>
+                        <div className="">{/* <Products images={imageData} /> */}</div>
+                    </div>
+                </div>
+            </div>
+        </ContentLayout>
+    );
+};
+
+export default ProductDetail;
