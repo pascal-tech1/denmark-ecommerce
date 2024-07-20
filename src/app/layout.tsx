c9b685c3d1b8f3a7fe3d25c9468f1be821c4c020
Pascal <pascalazubike003@gmail..com>
2024-07-20 13:57:26 -0700

    turned productdetail page to server component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ import AdminPanelLayout from "@/components/admin-panel/admin-panel-layout";
 import { Toaster } from "@/components/ui/toaster";
 
 
-export const metadata: Metadata = {
+const metadata: Metadata = {
   metadataBase: new URL(
     process.env.APP_URL
       ? `${process.env.APP_URL}`
@@ -16,39 +16,69 @@ export const metadata: Metadata = {
         ? `https://${process.env.VERCEL_URL}`
         : `http://localhost:${process.env.PORT || 3000}`
   ),
-  title: "Men's Fashion Store",
+  title: "Denmark Single Product Page",
   description:
-    "Discover the latest in men's fashion at our online store. Shop a wide range of clothing, accessories, and more, all with free shipping.",
+    "just found this wonderfull product available on denmarkmulbizltd.",
   alternates: {
     canonical: "/"
   },
   openGraph: {
-    url: "/",
-    title: "Denmark Men's Fashion Store",
+    url: "https://www.denmarkmultibuzltd.com/productdetail/669837055c645243fae405cc",
+    title: "title in openGraph",
     description:
-      "Discover the latest in men's fashion at our online store. Shop a wide range of clothing, accessories, and more, all with free shipping.",
+      " accessories, and more, all with free shipping.",
     type: "website",
-    images: [
-      {
-        url: "https://www.denmarkmultibuzltd.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fbannerone.f814d19f.png&w=3840&q=75",
-        width: 1200,
-        height: 630,
-        alt: "Men's Fashion Store"
-      }
-    ]
+    images:
+    {
+      url: 'https://www.denmarkmultibuzltd.com/_next/image?url=https%3A%2F%2Fres.cloudinary.com%2Fdztt3ldiy%2Fimage%2Fupload%2Fv1721251502%2FWhatsApp_Image_2024-07-09_at_4.50.59_AM_snpybj.jpg&w=640&q=75',
+      width: 1200,
+      height: 630,
+      alt: "Men's Fashion Store"
+    }
+
   },
-  twitter: {
-    card: "summary_large_image",
-    title: "Men's Fashion Store",
-    description:
-      "Discover the latest in men's fashion at our online store. Shop a wide range of clothing, accessories, and more, all with free shipping.",
-    images: [
-      {
-        url: "https://www.denmarkmultibuzltd.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fbannerone.f814d19f.png&w=3840&q=75",
-        alt: "Men's Fashion Store"
-      }
-    ]
-  }
+
+  // export const metadata: Metadata = {
+  //   metadataBase: new URL(
+  //     process.env.APP_URL
+  //       ? `${process.env.APP_URL}`
+  //       : process.env.VERCEL_URL
+  //         ? `https://${process.env.VERCEL_URL}`
+  //         : `http://localhost:${process.env.PORT || 3000}`
+  //   ),
+  //   title: "Men's Fashion Store",
+  //   description:
+  //     "Discover the latest in men's fashion at our online store. Shop a wide range of clothing, accessories, and more, all with free shipping.",
+  //   alternates: {
+  //     canonical: "/"
+  //   },
+  //   openGraph: {
+  //     url: "/",
+  //     title: "Denmark Men's Fashion Store",
+  //     description:
+  //       "Discover the latest in men's fashion at our online store. Shop a wide range of clothing, accessories, and more, all with free shipping.",
+  //     type: "website",
+  //     images: [
+  //       {
+  //         url: "https://www.denmarkmultibuzltd.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fbannerone.f814d19f.png&w=3840&q=75",
+  //         width: 1200,
+  //         height: 630,
+  //         alt: "Men's Fashion Store"
+  //       }
+  //     ]
+  //   },
+  //   twitter: {
+  //     card: "summary_large_image",
+  //     title: "Men's Fashion Store",
+  //     description:
+  //       "Discover the latest in men's fashion at our online store. Shop a wide range of clothing, accessories, and more, all with free shipping.",
+  //     images: [
+  //       {
+  //         url: "https://www.denmarkmultibuzltd.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fbannerone.f814d19f.png&w=3840&q=75",
+  //         alt: "Men's Fashion Store"
+  //       }
+  //     ]
+  //   }
 };
 
 export default function RootLayout({
--- src/app/productdetail/[id]/page.tsx
+++ src/app/productdetail/[id]/page.tsx
@@ -1,28 +1,47 @@
 import ProductDetail from "@/components/admin-panel/productDetailComp";
 import type { Metadata } from "next";
-import Head from "next/head";
 
 
 
 export default function ProductDetailPage() {
 
 
+  const metadata: Metadata = {
+    metadataBase: new URL(
+      process.env.APP_URL
+        ? `${process.env.APP_URL}`
+        : process.env.VERCEL_URL
+          ? `https://${process.env.VERCEL_URL}`
+          : `http://localhost:${process.env.PORT || 3000}`
+    ),
+    title: "Denmark Single Product Page",
+    description:
+      "just found this wonderfull product available on denmarkmulbizltd.",
+    alternates: {
+      canonical: "/"
+    },
+    openGraph: {
+      url: "https://www.denmarkmultibuzltd.com/productdetail/669837055c645243fae405cc",
+      title: "title in openGraph",
+      description:
+        " accessories, and more, all with free shipping.",
+      type: "website",
+      images:
+      {
+        url: 'https://www.denmarkmultibuzltd.com/_next/image?url=https%3A%2F%2Fres.cloudinary.com%2Fdztt3ldiy%2Fimage%2Fupload%2Fv1721251502%2FWhatsApp_Image_2024-07-09_at_4.50.59_AM_snpybj.jpg&w=640&q=75',
+        width: 1200,
+        height: 630,
+        alt: "Men's Fashion Store"
+      }
 
+    },
+
+
+
+  };
   return (
 
     <div>
-      <Head>
-        <meta name="msapplication-TileImage" content="https://www.denmarkmultibuzltd.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fbannerone.f814d19f.png&w=3840&q=75" />
-        <meta property="og:site_name" content="Pure Surface" />
-        <meta property="og:title" content="225 Days of Self Sanitizing Surface from Coronavirus" />
-        <meta property="og:description" content="Just 1 time application" />
-        <meta property="og:image" itemProp="image" content="https://www.denmarkmultibuzltd.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fbannerone.f814d19f.png&w=3840&q=75" />
-        <meta property="og:type" content="website" />
-        <meta property="og:image:type" content="image/png" />
-        <meta property="og:image:width" content="300" />
-        <meta property="og:image:height" content="300" />
-        <meta property="og:url" content="https://www.denmarkmultibuzltd.com/productdetail/6696e7b51043a84424903d2c" />
-      </Head>
       <ProductDetail />
     </div>
   );
