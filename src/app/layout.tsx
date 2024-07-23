b73be37d93ebe83c6c738ee7a025341ef518f356
Pascal <pascalazubike003@gmail..com>
2024-07-23 06:03:58 -0700

    modify home page head tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,47 +6,48 @@ import "./globals.css";
 import { ThemeProvider } from "@/providers/theme-provider";
 import AdminPanelLayout from "@/components/admin-panel/admin-panel-layout";
 import { Toaster } from "@/components/ui/toaster";
+import Head from "next/head";
 
-export const metadata: Metadata = {
-  metadataBase: new URL(
-    process.env.APP_URL
-      ? `${process.env.APP_URL}`
-      : process.env.VERCEL_URL
-      ? `https://${process.env.VERCEL_URL}`
-      : `http://localhost:${process.env.PORT || 3000}`
-  ),
-  title: "Men's Fashion Store",
-  description:
-    "Discover the latest in men's fashion at our online store. Shop a wide range of clothing, accessories, and more, all with free shipping in nigeria ekiti Ado ekiti.",
-  alternates: {
-    canonical: "/"
-  },
-  openGraph: {
-    url: "/",
-    title: "Men's Fashion Store",
-    description:
-      "Discover the latest in men's fashion at our online store. Shop a wide range of clothing, accessories, and more, all with free shipping in nigeria ekiti Ado ekiti.",
-    type: "website",
-    images: {
-      url: "https://www.denmarkmultibuzltd.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fbannerone.f814d19f.png&w=3840&q=75",
-      width: 1200,
-      height: 630,
-      alt: "Men's Fashion Store"
-    }
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: "Men's Fashion Store",
-    description:
-      "Discover the latest in men's fashion at our online store. Shop a wide range of clothing, accessories, and more, all with free shipping in nigeria ekiti Ado ekiti.",
-    images: {
-      url: "https://www.denmarkmultibuzltd.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fbannerone.f814d19f.png&w=3840&q=75",
-      width: 1200,
-      height: 630,
-      alt: "Men's Fashion Store"
-    }
-  }
-};
+// export const metadata: Metadata = {
+//   metadataBase: new URL(
+//     process.env.APP_URL
+//       ? `${process.env.APP_URL}`
+//       : process.env.VERCEL_URL
+//       ? `https://${process.env.VERCEL_URL}`
+//       : `http://localhost:${process.env.PORT || 3000}`
+//   ),
+//   title: "Men's Fashion Store",
+//   description:
+//     "Discover the latest in men's fashion at our online store. Shop a wide range of clothing, accessories, and more, all with free shipping in nigeria ekiti Ado ekiti.",
+//   alternates: {
+//     canonical: "/"
+//   },
+//   openGraph: {
+//     url: "/",
+//     title: "Men's Fashion Store",
+//     description:
+//       "Discover the latest in men's fashion at our online store. Shop a wide range of clothing, accessories, and more, all with free shipping in nigeria ekiti Ado ekiti.",
+//     type: "website",
+//     images: {
+//       url: "https://www.denmarkmultibuzltd.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fbannerone.f814d19f.png&w=3840&q=75",
+//       width: 1200,
+//       height: 630,
+//       alt: "Men's Fashion Store"
+//     }
+//   },
+//   twitter: {
+//     card: "summary_large_image",
+//     title: "Men's Fashion Store",
+//     description:
+//       "Discover the latest in men's fashion at our online store. Shop a wide range of clothing, accessories, and more, all with free shipping in nigeria ekiti Ado ekiti.",
+//     images: {
+//       url: "https://www.denmarkmultibuzltd.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fbannerone.f814d19f.png&w=3840&q=75",
+//       width: 1200,
+//       height: 630,
+//       alt: "Men's Fashion Store"
+//     }
+//   }
+// };
 
 export default function RootLayout({
   children
@@ -56,6 +57,23 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={GeistSans.className}>
+        <Head>
+          <title>Cool Title</title>
+          <meta
+            name="description"
+            content="Checkout our cool page"
+            key="desc"
+          />
+          <meta property="og:title" content="Social Title for Cool Page" />
+          <meta
+            property="og:description"
+            content="And a social description for our cool page"
+          />
+          <meta
+            property="og:image"
+            content="https://example.com/images/cool-page.jpg"
+          />
+        </Head>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <AdminPanelLayout>{children}</AdminPanelLayout>
         </ThemeProvider>
