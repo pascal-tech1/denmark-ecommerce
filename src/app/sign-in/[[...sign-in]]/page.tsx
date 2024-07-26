ea0b52bb5b26a5713830bd617db62da62008bef3
Pascal <pascalazubike003@gmail..com>
2024-07-26 08:42:05 -0700

    removed opengraph image and svg images

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,36 +1,41 @@
-import { ContentLayout } from '@/components/admin-panel/content-layout';
-import PlaceholderContent from '@/components/demo/placeholder-content';
-import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from '@/components/ui/breadcrumb';
-import { SignIn } from '@clerk/nextjs';
-import { dark } from '@clerk/themes';
-import Link from 'next/link';
+import { ContentLayout } from "@/components/admin-panel/content-layout";
+import PlaceholderContent from "@/components/demo/placeholder-content";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator
+} from "@/components/ui/breadcrumb";
+import { SignIn } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
+import Link from "next/link";
 
 export default function Page() {
-    return (
-        <ContentLayout title="Sign Up">
-
-            <Breadcrumb>
-                <BreadcrumbList>
-                    <BreadcrumbItem>
-                        <BreadcrumbLink asChild>
-                            <Link href="/">Home</Link>
-                        </BreadcrumbLink>
-                    </BreadcrumbItem>
-                    <BreadcrumbSeparator />
-                    <BreadcrumbItem>
-                        <BreadcrumbPage>Register</BreadcrumbPage>
-                    </BreadcrumbItem>
-                </BreadcrumbList>
-            </Breadcrumb>
-            <PlaceholderContent>
-
-                <SignIn
-
-                    path="/sign-in"
-                    routing="path"
-                    signUpUrl="/sign-up"
-                />
-            </PlaceholderContent>
-        </ContentLayout>
-    );
+  return (
+    <ContentLayout title="Sign Up">
+      <Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink asChild>
+              <Link href="/">Home</Link>
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbPage>Register</BreadcrumbPage>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
+      <PlaceholderContent>
+        <SignIn
+          path="/sign-in"
+          routing="path"
+          signUpUrl="/sign-up"
+          forceRedirectUrl={"/"}
+        />
+      </PlaceholderContent>
+    </ContentLayout>
+  );
 }
--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -1,36 +1,41 @@
+import { SignUp } from "@clerk/nextjs";
+import { ContentLayout } from "@/components/admin-panel/content-layout";
+import PlaceholderContent from "@/components/demo/placeholder-content";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator
+} from "@/components/ui/breadcrumb";
 
-import { SignUp } from '@clerk/nextjs';
-import { ContentLayout } from '@/components/admin-panel/content-layout';
-import PlaceholderContent from '@/components/demo/placeholder-content';
-import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from '@/components/ui/breadcrumb';
-
-import Link from 'next/link';
+import Link from "next/link";
 
 export default function Page() {
-    return (
-        <ContentLayout title="Sign Up">
-
-            <Breadcrumb>
-                <BreadcrumbList>
-                    <BreadcrumbItem>
-                        <BreadcrumbLink asChild>
-                            <Link href="/">Home</Link>
-                        </BreadcrumbLink>
-                    </BreadcrumbItem>
-                    <BreadcrumbSeparator />
-                    <BreadcrumbItem>
-                        <BreadcrumbPage>Register</BreadcrumbPage>
-                    </BreadcrumbItem>
-                </BreadcrumbList>
-            </Breadcrumb>
-            <PlaceholderContent>
-
-                <SignUp
-                    path="/sign-up"
-                    routing="path"
-                    signInUrl="/sign-in"
-                />
-            </PlaceholderContent>
-        </ContentLayout>
-    );
+  return (
+    <ContentLayout title="Sign Up">
+      <Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink asChild>
+              <Link href="/">Home</Link>
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbPage>Register</BreadcrumbPage>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
+      <PlaceholderContent>
+        <SignUp
+          path="/sign-up"
+          routing="path"
+          signInUrl="/sign-in"
+          signInForceRedirectUrl={"/"}
+        />
+      </PlaceholderContent>
+    </ContentLayout>
+  );
 }
