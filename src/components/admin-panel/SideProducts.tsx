ce5ca032e1fc9e134280637b19aa4e2d9437c0d1
Pascal <pascalazubike003@gmail..com>
2024-07-28 13:34:07 -0700

    fixed overflow x

--- src/components/admin-panel/SideProducts.tsx
+++ src/components/admin-panel/SideProducts.tsx
@@ -1,81 +1,85 @@
-"use client"
+"use client";
 import Image, { StaticImageData } from "next/image";
 import { useRouter } from "next/navigation";
 import React from "react";
 import { SideProductSkeletonLoading } from "./sideProductSkeletonLoading";
 import useFetchAllProductPaginated from "@/hooks/useFetchAllProductPaginated";
 import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
-import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "../ui/tooltip";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger
+} from "../ui/tooltip";
 
 const SideProducts = ({ heading }: { heading: string }) => {
   const router = useRouter();
-
 
   const handleCardClick = (_id: string) => {
     router.push(`/productdetail/${_id}`);
   };
 
-
-
-  const {
-    data,
-    error,
-    isPending,
-
-  } = useInfiniteQuery({
-    queryKey: ['sidePrducts', heading],
-    queryFn: ({ pageParam }: { pageParam: any }) => fetch(`/routes/fetchAllProducts?cursor=${pageParam}&limit=4&heading=${heading}`).then((res) => res.json()),
+  const { data, error, isPending } = useInfiniteQuery({
+    queryKey: ["sidePrducts", heading],
+    queryFn: ({ pageParam }: { pageParam: any }) =>
+      fetch(
+        `/routes/fetchAllProducts?cursor=${pageParam}&limit=4&heading=${heading}`
+      ).then((res) => res.json()),
     initialPageParam: 0,
     getNextPageParam: (lastPage, pages) => lastPage.nextCursor,
-    staleTime: 0,
-  })
-
-
+    staleTime: 0
+  });
 
   return (
-    <div>
-
-      {isPending ? <SideProductSkeletonLoading heading={heading} /> : <div className="flex flex-col gap-4 justify-center rounded-lg">
-        <div className="flex items-center text-center drop-shadow-lg pt-6 font-bold flex-col">
-          {heading}
-          <div className="border-b border-b-yellow-400 border-opacity-15 w-28 pt-2"></div>
-        </div>
-        <div className="">
-          {data?.pages[0]?.products?.map((product: any, index: number) => (
-            <div
-              key={index}
-              className="w-full group py-4 px-2 flex gap-4 items-center"
-            >
+    <div className=" overflow-x-hidden">
+      {isPending ? (
+        <SideProductSkeletonLoading heading={heading} />
+      ) : (
+        <div className="flex flex-col gap-4 justify-center rounded-lg">
+          <div className="flex items-center text-center drop-shadow-lg pt-6 font-bold flex-col">
+            {heading}
+            <div className="border-b border-b-yellow-400 border-opacity-15 w-28 pt-2"></div>
+          </div>
+          <div className="">
+            {data?.pages[0]?.products?.map((product: any, index: number) => (
               <div
-                onClick={() => handleCardClick(product._id)}
-                className="relative bg-gray-300 overflow-hidden cursor-pointer rounded-md h-16 w-16 flex-shrink-0"
+                key={index}
+                className="w-full group py-4 px-2 flex gap-4 items-center"
               >
-                <Image
-                  src={product.imageUrl}
-                  alt={product.title}
-                  layout="fill"
-                  blurDataURL={`data:image/jpeg;base64,${product.blurImage}`}
-                  placeholder="blur"
-                  className="object-cover group-hover:scale-105 rounded-md"
-                />
-              </div>
-              <div className=" flex flex-col gap-2 justify-between ">
-                <h2
+                <div
                   onClick={() => handleCardClick(product._id)}
-                  className=" hover:text-yellow-500 self-start items-start dark:hover:text-yellow-100 cursor-pointer transition-transform duration-300 truncate whitespace-nowrap overflow-hidden text-ellipsis"
+                  className="relative bg-gray-300 overflow-hidden cursor-pointer rounded-md h-16 w-16 flex-shrink-0"
                 >
-                  {product.title}
-                </h2>
-                <p className=" whitespace-nowrap text-gray-800 dark:text-neutral-400">
-                  Price: &#8358; {product.price}
-                </p>
+                  <Image
+                    src={product.imageUrl}
+                    alt={product.title}
+                    layout="fill"
+                    blurDataURL={`data:image/jpeg;base64,${product.blurImage}`}
+                    placeholder="blur"
+                    className="object-cover group-hover:scale-105 rounded-md"
+                  />
+                </div>
+                <div className=" flex flex-col gap-2 justify-between ">
+                  <div>
+                    <h2
+                      onClick={() => handleCardClick(product._id)}
+                      className="dark:text-neutral-200  mb-2 hover:text-yellow-500 dark:hover:text-yellow-100 cursor-pointer transition-transform duration-300 truncate whitespace-nowrap overflow-hidden text-ellipsis"
+                    >
+                      {product.title}
+                    </h2>
+                  </div>
+                  <p className=" whitespace-nowrap text-gray-800 dark:text-neutral-400">
+                    Price: &#8358; {product.price}
+                  </p>
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
+          {error && (
+            <h1 className=" ml-10">failed to fetch Products try again </h1>
+          )}
         </div>
-        {error && <h1 className=" ml-10">failed to fetch Products try again </h1>}
-      </div>}
-
+      )}
     </div>
   );
 };
